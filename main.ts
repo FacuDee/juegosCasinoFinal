3bf91cc9dc2938948137710317f3f922982cdada
lucaslapla <lucaslapla@hotmail.com>
2024-12-01 14:38:15 -0300

    Se sube archivo Casino..

--- main.ts
+++ main.ts
@@ -5,10 +5,8 @@ import { Jugador } from "./Jugador";
 import { JuegoCasino } from "./JuegoCasino";
 import { Dados } from "./Dados";
 import { BlackJack } from "./BlackJack";
-import { Ruleta } from "./Ruleta";
+import { Ruleta } from "./Ruleta/Ruleta";
 import { TragamonedasPlus } from "./TragamonedasPlus";
-
-
 
 
 const arrJugadores: Jugador[] = [
@@ -23,8 +21,8 @@ const arrJugadores: Jugador[] = [
 const dados :Dados = new Dados ("Dados",10,"Natural, gana sacando 7 u 11 en el primer tiro. Craps pierde si saca 2,3 u 12 en el primer tiro.. sino tira hasta que repita el prumer tiro' Gana' o saque 7 y pierde lo que suceda primero",0,0,0);
 const tragamonedasClasico: TragamonedasClasico = new TragamonedasClasico();
 const tragamonedasPlus: TragamonedasPlus = new TragamonedasPlus();
-const ruleta: Ruleta = new Ruleta("Ruleta",10, "En un tablero de 36 numeros, repartidos entre rojos, Negros y 0 verde, elige un numero o color, si acierta Gana",100,10,10);
-const blackJack: BlackJack  = new BlackJack("Black Jack",9, "Obtén un puntaje lo más cercano a 21 sin pasarte, superando al crupier.",[],[],[]);
+const ruleta: Ruleta = new Ruleta(15,15,0);
+const blackJack: BlackJack  = new BlackJack(9, arrJugadores[4]);
 //********Se construyen las variables de Cadajuego con sus parametros******* */
 
 const arrJuegos : JuegoCasino []=[];
